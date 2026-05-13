78aafb48f63500ecbab3db96e01cf6559e81edaa
Saurabh-Taylor <tailorsaurabh12@gmail.com>
2026-05-13 19:03:39 +0530

    Add initial implementation of RAG API with Redis queue integration and FastAPI setup

---
+++ udemy-lectures-genai-agent-ai/gen-ai/rag_async/queues/worker.py
@@ -0,0 +1,49 @@
+from openai import OpenAI
+from langchain_openai import OpenAIEmbeddings
+from langchain_qdrant import QdrantVectorStore
+from dotenv import load_dotenv
+load_dotenv()
+
+openai_client = OpenAI()
+
+# Vector embedding model
+embedding_model = OpenAIEmbeddings(
+    model="text-embedding-3-large"
+)
+
+# connect vector db connection
+vector_db =  QdrantVectorStore.from_existing_collection(
+    url="http://localhost:6333",
+    collection_name="nodejs-interview-questions",
+    embedding=embedding_model
+)
+
+
+def process_query(query: str):
+    print("Searching Chunks", query)
+    search_results = vector_db.similarity_search(query=query)
+
+    context = "\n\n\n".join([f"Page {result.metadata.get('page', 'N/A')}:\n{result.page_content}" for result in search_results])
+
+    SYSTEM_PROMPT = f"""
+        You are a helpful assistant that answers user query based on the available context retrieved from the database along with page_contents and page_number. Always cite the page number in your answer. If you don't know the answer, say you don't know.
+
+        Context:
+        {context}
+    """
+    responses = openai_client.chat.completions.create(
+        model="gpt-5",
+        messages=[
+            {
+                "role": "system",
+                "content": SYSTEM_PROMPT
+            },
+            {
+                "role": "user",
+                "content": query
+            }
+        ]
+    )
+    print("response::", responses.choices[0].message.content)
+
+    return responses.choices[0].message.content
